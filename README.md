6e9b8b7814b8da83454990dba94f7c60fddcf58c
aDiZmalCode <deferdinand@gmail.com>
2023-07-04 13:52:25 -0400

    HEAD

---
+++ README.md
@@ -0,0 +1,4 @@
+# Vector-HS
+A web app to collect various data related to public health
+Google Clasp, Parcel.Js were added to the project to send it to Google App Scripts.
+The aim of this project is a userform to collect GeoSpatial Data among other Public Health data.
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>{{ title }}</div>    
+</template>
+  
+  <script>
+
+    // import Map from './Components/Map'
+
+    export default {
+      
+      components: {
+          // Map
+        },
+
+      data() {
+        return {
+          title: "here we go again",
+        }
+      },
+    }
+  </script>
